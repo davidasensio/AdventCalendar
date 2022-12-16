5e76660422a3124b9d4476e2c39a310b4f649b3d
David Asensio <davidasensio81@gmail.com>
2022-12-16 18:09:46 +0100

    📅 Day 11 - Part 2 (Not working)

--- src/main/kotlin/days2022/Day11.kt
+++ src/main/kotlin/days2022/Day11.kt
@@ -8,12 +8,13 @@ fun main() {
 
 object Day11 : Day(11, "Monkey in the Middle", true) {
     private val monkeys: List<Monkey>
+    val rounds = 20
 
     init {
         monkeys = inputString.split("\n\n").map {
             val lines = it.split("\n")
             Monkey(
-                items = "Starting items: (.*)".regexFind(lines[1]).split(", ").map { it.toInt() }.toMutableList(),
+                items = "Starting items: (.*)".regexFind(lines[1]).split(", ").map { it.toLong() }.toMutableList(),
                 operation = "Operation: new = old (.*)".regexFind(lines[2]),
                 divider = "Test: divisible by (.*)".regexFind(lines[3]).toInt(),
                 monkeyTrueRedirect = "If true: throw to monkey (.*)".regexFind(lines[4]).toInt(),
@@ -23,14 +24,12 @@ object Day11 : Day(11, "Monkey in the Middle", true) {
     }
 
     override fun partOne(): Any {
-        val rounds = 20
-
         repeat(rounds) {
             monkeys.forEach { monkey ->
                 while (monkey.items.isNotEmpty()) {
                     val item = monkey.items.removeFirst()
                     val (operator, factor2Str) = monkey.operation.split(" ")
-                    val factor2 = if (factor2Str == "old") item else factor2Str.toInt()
+                    val factor2 = if (factor2Str == "old") item else factor2Str.toLong()
                     val worryLevel = when (operator) {
                         "-" -> item - factor2
                         "+" -> item + factor2
@@ -38,7 +37,7 @@ object Day11 : Day(11, "Monkey in the Middle", true) {
                         else -> item / factor2
                     }
                     val worryLevelDivided = worryLevel / 3
-                    val isDivisible = worryLevelDivided % monkey.divider == 0
+                    val isDivisible = worryLevelDivided % monkey.divider == 0L
                     val receiver = if (isDivisible) monkey.monkeyTrueRedirect else monkey.monkeyFalseRedirect
                     monkeys[receiver].items.add(worryLevelDivided)
                     monkey.counter++
@@ -53,18 +52,42 @@ object Day11 : Day(11, "Monkey in the Middle", true) {
 
     override fun partTwo(): Any {
         return false
+        repeat(rounds) {
+            monkeys.forEach { monkey ->
+                while (monkey.items.isNotEmpty()) {
+                    val item = monkey.items.removeFirst()
+                    val (operator, factor2Str) = monkey.operation.split(" ")
+                    val factor2 = if (factor2Str == "old") item else factor2Str.toLong()
+                    val worryLevel = when (operator) {
+                        "-" -> item - factor2
+                        "+" -> item + factor2
+                        "*" -> item * factor2
+                        else -> item / factor2
+                    }
+                    val worryLevelDivided = worryLevel % monkey.divider
+                    val isDivisible = worryLevelDivided % monkey.divider == 0L
+                    val receiver = if (isDivisible) monkey.monkeyTrueRedirect else monkey.monkeyFalseRedirect
+                    monkey.counter++
+                    monkeys[receiver].items.add(worryLevelDivided)
+                }
+            }
+        }
+        logLn(monkeys.map { it.counter }.toString())
+        val sortedMonkeys = monkeys.sortedByDescending { it.counter }
+        val monkeyBusinessLevel = sortedMonkeys[0].counter * sortedMonkeys[1].counter
+        return "Monkey Business Level is $monkeyBusinessLevel"
     }
 
     private fun String.regexFind(text: String) =
         this.toRegex().find(text)?.groups?.last()?.value ?: ""
 
     data class Monkey(
-        val items: MutableList<Int>,
+        val items: MutableList<Long>,
         val operation: String,
         val divider: Int,
         val monkeyTrueRedirect: Int,
         val monkeyFalseRedirect: Int,
-        var counter: Int = 0
+        var counter: Long = 0
     )
 }
 
